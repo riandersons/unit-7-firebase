22fc849a6e2d11d9bb86dad9f3c2f8ac11373a39
Robert Anderson <riandersons@cox.net>
2019-07-08 20:26:03 -0700

    Format html, initial js logic started

---
+++ assets/javascript/logic.js
@@ -0,0 +1,180 @@
+
+
+
+
+
+
+// ------------------------------
+// 
+// Steps to complete:
+
+// 1. Initialize Firebase
+// 2. Create button for adding new employees - then update the html + update the database
+// 3. Create a way to retrieve employees from the employee database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
+
+// 1. Initialize Firebase
+const config = {
+    apiKey: "AIzaSyA_QypGPkcjPtylRDscf7-HQl8ribnFeIs",
+    authDomain: "time-sheet-55009.firebaseapp.com",
+    databaseURL: "https://time-sheet-55009.firebaseio.com",
+    storageBucket: "time-sheet-55009.appspot.com"
+  };
+  
+  firebase.initializeApp(config);
+  
+  const database = firebase.database();
+  
+  // 2. Button for adding Employees
+  $("#add-employee-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    const empName = $("#employee-name-input").val().trim();
+    const empRole = $("#role-input").val().trim();
+    const empStart = moment($("#start-input").val().trim(), "MM/DD/YYYY").format("X");
+    const empRate = $("#rate-input").val().trim();
+  
+    // Creates local "temporary" object for holding employee data
+    const newEmp = {
+      name: empName,
+      role: empRole,
+      start: empStart,
+      rate: empRate
+    };
+  
+    // Uploads employee data to the database
+    database.ref().push(newEmp);
+  
+    // Logs everything to console
+    console.log(newEmp.name);
+    console.log(newEmp.role);
+    console.log(newEmp.start);
+    console.log(newEmp.rate);
+  
+    alert("Employee successfully added");
+  
+    // Clears all of the text-boxes
+    $("#employee-name-input").val("");
+    $("#role-input").val("");
+    $("#start-input").val("");
+    $("#rate-input").val("");
+  });
+  
+  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    const empName = childSnapshot.val().name;
+    const empRole = childSnapshot.val().role;
+    const empStart = childSnapshot.val().start;
+    const empRate = childSnapshot.val().rate;
+  
+    // Employee Info
+    console.log(empName);
+    console.log(empRole);
+    console.log(empStart);
+    console.log(empRate);
+  
+    // Prettify the employee start
+    let empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+  
+    // Calculate the months worked using hardcore math
+    // To calculate the months worked
+    let empMonths = moment().diff(moment(empStart, "X"), "months");
+    console.log(empMonths);
+  
+    // Calculate the total billed rate
+    let empBilled = empMonths * empRate;
+    console.log(empBilled);
+  
+    // Create the new row
+    let newRow = $("<tr>").append(
+      $("<td>").text(empName),
+      $("<td>").text(empRole),
+      $("<td>").text(empStartPretty),
+      $("<td>").text(empMonths),
+      $("<td>").text(empRate),
+      $("<td>").text(empBilled)
+    );
+  
+    // Append the new row to the table
+    $("#employee-table > tbody").append(newRow);
+  });
+  
+// ------------------------------------------------------------------------------
+  // train time portion
+  // Example Time Math
+  // -----------------------------------------------------------------------------
+  // Assume Employee start date of January 1, 2015
+  // Assume current date is March 1, 2016
+  
+  // We know that this is 15 months.
+  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
+  
+    // Assume the following situations.
+
+    // (TEST 1)
+    // First Train of the Day is 3:00 AM
+    // Assume Train comes every 3 minutes.
+    // Assume the current time is 3:16 AM....
+    // What time would the next train be...? (Use your brain first)
+    // It would be 3:18 -- 2 minutes away
+
+    // (TEST 2)
+    // First Train of the Day is 3:00 AM
+    // Assume Train comes every 7 minutes.
+    // Assume the current time is 3:16 AM....
+    // What time would the next train be...? (Use your brain first)
+    // It would be 3:21 -- 5 minutes away
+
+
+    // ==========================================================
+
+    // Solved Mathematically
+    // Test case 1:
+    // 16 - 00 = 16
+    // 16 % 3 = 1 (Modulus is the remainder)
+    // 3 - 1 = 2 minutes away
+    // 2 + 3:16 = 3:18
+
+    // Solved Mathematically
+    // Test case 2:
+    // 16 - 00 = 16
+    // 16 % 7 = 2 (Modulus is the remainder)
+    // 7 - 2 = 5 minutes away
+    // 5 + 3:16 = 3:21
+
+    // Assumptions
+    let tFrequency = 3;
+
+    // Time is 3:30 AM
+    let firstTime = "03:30";
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    let firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    let currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    let tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    let tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    let nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
